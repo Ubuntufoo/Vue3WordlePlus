26dca78f00616627542bea9f89371ef5b257b5eb
Timothy Murphy <TimothyMurphy123@gmail.com>
2023-04-27 22:40:28 -0400

    finished, with 1 quirk of backspace remaining.

--- src/components/Wordle/WordleMain.vue
+++ src/components/Wordle/WordleMain.vue
@@ -1,8 +1,7 @@
 <!-- eslint-disable no-unused-vars -->
 <script setup>
-import { onMounted, reactive, computed, watch, defineProps } from "vue";
+import { onMounted, reactive, computed, defineProps, watchEffect } from "vue";
 import axios from 'axios';
-import VueAxios from 'vue-axios';
 import SimpleKeyboard from "./WordleKeyboard.vue"
 import WordleRow from "./WordleRow.vue";
 import WordleGameOver from "./WordleGameOver.vue";
@@ -10,7 +9,6 @@ import WordleGameOver from "./WordleGameOver.vue";
 const props = defineProps({
   user: String
 });
-
 const state = reactive({
   solution: "",
   guesses: [
@@ -28,13 +26,13 @@ const state = reactive({
     hint: [],
   }
 });
-
 const wonGame = computed(
   () => state.guesses[state.currentGuessIndex - 1] && state.guesses[state.currentGuessIndex - 1].join("") === state.solution,
 );
 const lostGame = computed(
   () => !wonGame.value && state.currentGuessIndex >= 6
 );
+
 const computedClass = computed(() => {
   let className = '';
   if (wonGame.value || lostGame.value) {
@@ -43,37 +41,19 @@ const computedClass = computed(() => {
   return className;
 });
 
-// watch(
-//   () => state.guesses,
-//   (newGuesses) => {
-//     for (let i = 0; i < state.guesses.length; i++) {
-//       state.guessesSplit[i] = state.guesses[i].split("");
-//     }
-//   },
-//   { deep: true }
-// );
-
 const handleInput = (key) => {
-
   if (state.currentGuessIndex >= 6 || wonGame.value) {
-    console.log("Victory!")
     return;
   }
   let currentGuess = state.guesses[state.currentGuessIndex]
-
   if (key == "{enter}") {
-    if (currentGuess.length >= 5) {
+    if (currentGuess.length >= 5 && !currentGuess.includes("")) {
       state.currentGuessIndex++;
-      for (var i = 0; i < currentGuess.length; i++) {
+      for (var i = 0; i < 5; i++) {
         let c = currentGuess[i];
         if (c == state.solution.charAt(i)) {
           state.guessedLetters.found.push(c);
-          const charIndex = state.solution.indexOf(c);
-          state.guesses.forEach((guess, x) => {
-            state.guesses[x][charIndex] = c;
-            console.log(state.guesses[x].indexOf(c, charIndex));
-
-          });
+          state.guesses[state.currentGuessIndex][i] = c;
         } else if (state.solution.indexOf(c) != -1) {
           state.guessedLetters.hint.push(c);
         } else {
@@ -82,21 +62,42 @@ const handleInput = (key) => {
       }
     }
   } else if (key == "{bksp}") {
-    // Remove most recent non-empty string from currentGuess array
     for (let i = currentGuess.length - 1; i >= 0; i--) {
-      if (currentGuess[i] !== "" && !state.guessedLetters.found.includes(currentGuess[i])) {
+      console.log(">>Backspace loop<<. currentGuess array value at index", i, "=", currentGuess[i], ". Is", currentGuess[i], "included in state.guessedLetters.found? True or false:", Boolean(state.guessedLetters.found.includes(currentGuess[i])), "Also, does currentGuess[i] == state.solution[i]? True or false:", Boolean(currentGuess[i] == state.solution[i]));
+      if (currentGuess[i] == "") {
+        console.log("Option 1. Its an empty", typeof (currentGuess[i]), ". continue.")
+        continue;
+      } else if (state.guessedLetters.found.includes(currentGuess[i]) && currentGuess[i] == state.solution[i]) {
+
+        console.log("Option 2: It matches solution @index. Number of occurs in last guess:", state.guesses[state.currentGuessIndex - 1].reduce((acc, curr) => acc + (curr === currentGuess[i]), 0), ". Number of occurs in solution:", state.solution.split("").reduce((acc, curr) => acc + (curr === currentGuess[i]), 0));
+
+        if (state.guesses[state.currentGuessIndex - 1].reduce((acc, curr) => acc + (curr === currentGuess[i]), 0) >= 2 && state.solution.split("").reduce((acc, curr) => acc + (curr === currentGuess[i]), 0) >= 2) {
+          console.log("Option 2.1: The previous guess and the solution have 2+ of the matching character. Ignore char and continue.");
+          continue;
+        } else if (state.guesses[state.currentGuessIndex - 1].reduce((acc, curr) => acc + (curr === currentGuess[i]), 0) <= 1 && state.solution.split("").reduce((acc, curr) => acc + (curr === currentGuess[i]), 0) >= 2) {
+          if (currentGuess.reduce((acc, curr) => acc + (curr === currentGuess[i]), 0) >= 2) {
+            console.log("Option 2.2.1: The solution has 2+ of the matching char, but the prev guess does not. Replacing char with '', as long as there is 1 still remaining in currentGuess.");
+            currentGuess[i] = "";
+            break;
+          }
+          console.log("Option 2.2.2: The solution has 2+ of the matching char, but the prev guess does not. Also, the currentGuess only has 1 instance of char remaining. Do nothing and continue.");
+          continue;
+        } else {
+          console.log("Option 2.3: Continue.");
+          continue;
+        }
+      } else {
+        console.log("Option 3. Make this index = ''.")
         currentGuess[i] = "";
         break;
       }
     }
-  } else if (currentGuess[state.currentGuessIndex].length < 5) {
-    //ADD LETTER IF ALPHABETICAL
+  } else if (key.length == 1) {
     const alphaRegex = /[a-zA-Z]/;
     if (alphaRegex.test(key)) {
       for (let i = 0; i < 5; i++) {
         if (!currentGuess[i]) {
           currentGuess[i] = key;
-          console.log("🚀 ~ file: WordleMain.vue:92 ~ handleInput ~ currentGuess:", currentGuess);
           break;
         }
       }
